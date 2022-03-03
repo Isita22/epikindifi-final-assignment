2b4a734d1c99d3a00a7523e5cd48a514003f989e
Isita Mohapatra <1828099@kiit.ac.in>
2022-03-03 19:36:26 +0530

    committ2

--- library.js
+++ library.js
@@ -1,43 +1,65 @@
 // Write your code here!
-let i=7
-function row()
-{
-    
-    var MyTable=document.getElementById('info-table')
-    var newRow=MyTable.insertRow(-1)
-    var newcell1=newRow.insertCell(0)
-    var newcell2=newRow.insertCell(1)
-    var newcell3=newRow.insertCell(2)
-    var newcell4=newRow.insertCell(3)
-    var newcell5=newRow.insertCell(4)
-    var newcell6=newRow.insertCell(5)
-    newcell1.innerHTML=i+1
+let i = 6
+var lendername=''
+function row() {
+
+    var MyTable = document.getElementById('formbody')
+    var newRow = MyTable.insertRow(-1)
+    var newcell1 = newRow.insertCell(0)
+    var newcell2 = newRow.insertCell(1)
+    var newcell3 = newRow.insertCell(2)
+    var newcell4 = newRow.insertCell(3)
+    var newcell5 = newRow.insertCell(4)
+    var newcell6 = newRow.insertCell(5)
+    newcell1.innerHTML = i + 1
     i++
-    newcell2.innerHTML=book.bookname
-    newcell3.innerHTML=book.authorname
+    newcell2.innerHTML = book.bookname
+    newcell3.innerHTML = book.authorname
+    newcell4.innerHTML=lendername
+
 
 }
 
 
-let book={bookname:"",
-authorname:""}
-const form=document.getElementById('form');
+let book = {
+    bookname: "",
+    authorname: ""
+}
+const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
-    // stop form submission
+
     event.preventDefault();
 
-    console.log(event.target[0].value,event.target[1].value)
-    book.bookname=event.target[0].value
-    book.authorname=event.target[1].value
+    book.bookname = event.target[0].value
+    book.authorname = event.target[1].value
+    form.reset()
     row()
 
 });
 
+document.getElementById('addbook').style.display = "none";
 
+const login = document.getElementById("login")
+login.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const loggedinuser = document.getElementById("logged-in-user-name")
+    if (e.target[0].value == '') {
+        loggedinuser.innerHTML = 'No user logged in'
+        document.getElementById('addbook').style.display = "none";
+    }
+    if (e.target[0].value == 'UserA' || e.target[0].value == 'UserB' || e.target[0].value == 'UserC' || e.target[0].value == 'UserD') 
+    {
+        loggedinuser.innerHTML = `Logged in user: ${e.target[0].value}`
+        document.getElementById('addbook').style.display = "";
+        const lendernamee=document.getElementById('addbook')
+        addbook.rows[0].cells[3].innerHTML=e.target[0].value
+        lendername=e.target[0].value
+    }
+    else {
+        loggedinuser.innerHTML = 'No user logged in'
+        document.getElementById('addbook').style.display = "none";
+    }
 
-// var button=document.getElementById("botton")
-// button.addEventListener("click",(event)=>{
-// row()
-// console.log("button")
-// })
+    login.reset()
+})
